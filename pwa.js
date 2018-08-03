0d306b857270d143af46ad3dcd6af2437afa707b
prateek951 <prince.cenation@gmail.com>
2018-08-03 19:35:48 +0530

    pwa theory

---
+++ pwa.js
@@ -0,0 +1,47 @@
+//  What are Progressive Web applications ? 
+
+//Progressively enhance web apps to look and feel like native apps 
+
+//Application working offline sync data in background and accessing camera features 
+//and all 
+
+//For older browsers compatibility we want to progressively enhance the performance of the application
+
+//load fast working pffline are the features that we want in the PWA
+
+// Initial load must be fast we dont want longer loading times 
+
+//Fast respond quickly to user actions 
+
+// Push notifications and all like features
+
+//WHY PWA ? ? 
+
+//Mobile web vrs. Native Applications 
+
+// 87% native applications and 13 % mobile web applications 
+
+//Push notifications bring the users back to the application 
+//Home screen icons make access easy for native applications.
+//Access native device features like camera and geolocation api 
+//Possibly working offline 
+
+//AGAINST NATIVE APPS 
+
+//learn two different languages like Swift and Android 
+
+//Native apps access device features leverage OS 
+// Top 3 apps win rest loses
+
+//Traditional web applications highly limited device feature access 
+// High reach no borders
+
+//PWAs combine best of both the worlds.High reach no borders since 
+// it is a webapp 
+
+//Go to the network tab and tick the offline mode to check 
+//whether an application is a PWA or not
+
+
+
+
